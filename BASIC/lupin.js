5d954f8144cd960259d47ffa82e82dc7e6a6f4a2
paradisel0st <doggiusdogg@gmail.com>
2026-06-16 00:21:44 -0300

    page, gbook, nav i think

--- BASIC/lupin.js
+++ BASIC/lupin.js
@@ -11,9 +11,12 @@ function start_news() {
             .join("<br>");
         e.style.visibility = "visible";
     };
-    x.open("GET", "FEED.txt", 1);
+    x.open("GET", (typeof ROOT_PATH !== 'undefined' ? ROOT_PATH : '') + "FEED.txt", 1);
     x.send();
 }
+
+var REL_ITEMS = [];
+var REL_PER_PAGE = 20;
 
 function rel_parse(txt) {
     var out = [], parts = txt.replace(/=begin[\s\S]*?=end/g, '').replace(/^;.*$/gm, '').split('<>'), i, n, s, l;
@@ -23,7 +26,44 @@ function rel_parse(txt) {
         l = parts[i + 2].trim();
         if (n) out.push({ n: n, s: s, l: l });
     }
-    return out;
+    return out.reverse();
+}
+
+function go_page(p) {
+    var tot = Math.ceil(REL_ITEMS.length / REL_PER_PAGE);
+    var start = (p - 1) * REL_PER_PAGE;
+    var s = '', i;
+    for (i = start; i < start + REL_PER_PAGE; i++) {
+        if (i < REL_ITEMS.length) {
+            s += '<tr><td>' + REL_ITEMS[i].n + '</td><td>' + REL_ITEMS[i].s + '</td><td>' + REL_ITEMS[i].l + '</td></tr>';
+        } else {
+            s += '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
+        }
+    }
+    
+    if (p === tot || tot === 0) {
+        s += '<tr><td colspan="3" id="cs">...AND MORE COMING SOON! TO A THEATRE NEAR YOU!!</td></tr>';
+    } else {
+        s += '<tr><td colspan="3" style="border: none;">&nbsp;</td></tr>';
+    }
+    
+    tot = Math.max(1, tot);
+    var pnav = '';
+    if (p > 1) pnav += '<a href="javascript:void(0)" onclick="go_page(1)" class="plnk">[First]</a> <a href="javascript:void(0)" onclick="go_page('+(p-1)+')" class="plnk">&lt;</a> ';
+    
+    for (i = 1; i <= tot; i++) {
+        if (i === p) {
+            pnav += '[ ' + i + ' ] ';
+        } else {
+            pnav += '[<a href="javascript:void(0)" onclick="go_page(' + i + ')" class="plnk">' + i + '</a>] ';
+        }
+    }
+    
+    if (p < tot) pnav += '<a href="javascript:void(0)" onclick="go_page('+(p+1)+')" class="plnk">&gt;</a> <a href="javascript:void(0)" onclick="go_page(' + tot + ')" class="plnk">[Last]</a>';
+    
+    s += '<tr><td colspan="3" style="text-align:center; padding:6px;">' + pnav + '</td></tr>';
+    
+    document.getElementById("rel-body").innerHTML = s;
 }
 
 function start_rel() {
@@ -31,14 +71,10 @@ function start_rel() {
     if (!e) return;
     x = new XMLHttpRequest();
     x.onload = function () {
-        var items = rel_parse(this.responseText), s = '', i;
-        for (i = 0; i < items.length; i++) {
-            s += '<tr><td>' + items[i].n + '</td><td>' + items[i].s + '</td><td>' + items[i].l + '</td></tr>';
-        }
-        s += '<tr><td colspan="3" id="cs">...AND MORE COMING SOON! TO A THEATRE NEAR YOU!!</td></tr>';
-        e.innerHTML = s;
+        REL_ITEMS = rel_parse(this.responseText);
+        go_page(1);
     };
-    x.open("GET", "RELEASE.TXT?t=" + Date.now(), 1);
+    x.open("GET", (typeof ROOT_PATH !== 'undefined' ? ROOT_PATH : '') + "RELEASE.TXT?t=" + Date.now(), 1);
     x.send();
 }
 
---
+++ BASIC/musumekko.js
@@ -0,0 +1,94 @@
+var PPG = 4; // posts per page
+
+function blog_strip(txt) {
+    // cm: ; lines, =begin...=end blocks
+    return txt.replace(/=begin[\s\S]*?=end/g, '').replace(/^;.*$/gm, '');
+}
+
+function blog_parse(txt) {
+    var out = [], parts = blog_strip(txt).split('<>'), i, t, d, h, c;
+    for (i = 0; i + 3 < parts.length; i += 4) {
+        t = parts[i].trim();
+        d = parts[i + 1].trim();
+        h = parts[i + 2].trim();
+        c = parts[i + 3].trim().replace(/\n/g, '<br>');
+        if (t) out.push({ t: t, d: d, h: h, c: c });
+    }
+    return out.reverse();
+}
+
+function blog_post(p) {
+    return '<div style="border:1px solid silver;padding:1px;margin-bottom:16px;">' +
+        '<div style="padding:4px;background:#EEEEEE;">' +
+        '<span style="font:bold 11px verdana;float:left;display:inline-block;max-width:73%;text-align:justify;">' + p.t + '</span>' +
+        '<span style="font:11px verdana;float:right;text-align:right;">' + p.d + ' ' + p.h + '</span>' +
+        '<div style="clear:both;"></div></div>' +
+        '<div style="padding:4px;background:#F9F9F9;font:11px verdana;text-align:justify;">' + p.c + '</div></div>';
+}
+
+function blog_hash(k) {
+    var m = location.hash.match(new RegExp(k + ';(\\d+)'));
+    return m ? parseInt(m[1], 10) : null;
+}
+
+var BLOG_ITEMS = [];
+
+function render_blog() {
+    var e = document.getElementById('bk');
+    if (!e) return;
+    
+    var pg = blog_hash('page') || 1;
+    var tot = Math.ceil(BLOG_ITEMS.length / PPG);
+    pg = Math.min(Math.max(1, pg), Math.max(1, tot));
+    
+    var s = '', i;
+    var start = (pg - 1) * PPG;
+    var end = Math.min(start + PPG, BLOG_ITEMS.length);
+    
+    for (i = start; i < end; i++) {
+        s += blog_post(BLOG_ITEMS[i]);
+    }
+    
+    tot = Math.max(1, tot);
+    var pnav = '';
+    
+    if (pg > 1) {
+        pnav += '<a href="#page;1" class="plnk">[First]</a> <a href="#page;' + (pg-1) + '" class="plnk">&lt;</a> ';
+    }
+    
+    for (i = 1; i <= tot; i++) {
+        if (i === pg) {
+            pnav += '[ ' + i + ' ] ';
+        } else {
+            pnav += '[<a href="#page;' + i + '" class="plnk">' + i + '</a>] ';
+        }
+    }
+    
+    if (pg < tot) {
+        pnav += '<a href="#page;' + (pg+1) + '" class="plnk">&gt;</a> <a href="#page;' + tot + '" class="plnk">[Last]</a>';
+    }
+    
+    var pnav_html = '<div style="text-align:center; padding:0 0 16px 0; font: 700 11px/100% \'Lucida Grande\', \'MS PGothic\', sans-serif;">' + pnav + '</div>';
+    
+    e.innerHTML = pnav_html + s;
+}
+
+function start_blog() {
+    var e = document.getElementById("bk"), x;
+    if (!e) return;
+    x = new XMLHttpRequest();
+    x.onload = function () {
+        BLOG_ITEMS = blog_parse(this.responseText);
+        render_blog();
+    };
+    x.open('GET', 'POST.TXT?t=' + Date.now(), true);
+    x.send();
+}
+
+window.addEventListener('hashchange', function () {
+    if (BLOG_ITEMS.length > 0) render_blog();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    start_blog();
+});
